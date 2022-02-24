e349717240501cf6c5dc32be7b2fcc88b9a96707
Gerald Bryant <25540747+geraldthedev@users.noreply.github.com>
2022-02-24 01:22:44 -0500

    Scroll

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,43 @@ export default function Home() {
         <meta name="description" content="Gerald Bryant's Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <header className={styles.banner}>
-      <div className={styles.overlay}></div>
+      <div className={styles.main}>
+             <div className={styles.overlay}></div>
+      <div className={styles.banner}>
+        <div className={styles.title}>
+        <p>Gerald Bryant</p>
+        </div>
+    
+  </div>
+  
 
-    </header>
+     
+
+      </div>
+      <div className={styles.info}>
+    Do consectetur minim id sint aliqua nisi qui reprehenderit dolore est irure ullamco velit. Commodo exercitation eu aute et culpa laboris irure ullamco veniam aliquip dolor fugiat anim. Incididunt reprehenderit ut laboris duis nulla laborum deserunt enim adipisicing reprehenderit ipsum proident pariatur ullamco. Irure anim duis elit ea qui commodo labore do amet proident et commodo cupidatat eu.
+
+Adipisicing pariatur ullamco fugiat eiusmod pariatur labore do labore magna nisi esse aliquip ipsum consectetur. Nostrud labore enim nisi officia nisi ullamco. Ex proident cupidatat pariatur do ea. Ullamco pariatur ea exercitation nisi deserunt consectetur duis minim Lorem.
+
+Occaecat irure aliqua ut labore laborum dolor voluptate sit exercitation pariatur. Ut deserunt incididunt ad ad consectetur elit dolor incididunt do. Voluptate quis proident in veniam cillum magna ipsum. Laborum ipsum aliquip ullamco elit do fugiat ad. Tempor proident in sunt non magna incididunt aute voluptate tempor id nulla esse occaecat. Ex consectetur labore do duis cupidatat do ullamco et irure exercitation irure minim in eu. Anim duis ad laboris duis ad cupidatat occaecat magna do laborum.
+
+Eiusmod ex dolore aliquip consequat laborum ad aliquip est exercitation eu veniam. Consectetur aliquip dolor officia reprehenderit cupidatat. Adipisicing esse est do ad adipisicing ex. Tempor voluptate adipisicing non consectetur magna enim consectetur est. Anim incididunt fugiat pariatur occaecat. Minim aliqua veniam quis voluptate ullamco ut tempor cupidatat.
+
+Ex id elit esse anim nulla et reprehenderit duis laborum nisi adipisicing culpa. Occaecat commodo enim velit velit officia tempor aliquip excepteur dolor. Labore elit laborum ea ea quis veniam officia cillum deserunt aliquip minim eu est. Culpa eiusmod ut aliquip minim elit aute nisi eiusmod aliqua sunt. Commodo sunt sunt incididunt in exercitation ullamco veniam eiusmod dolore et cillum. Eiusmod magna cupidatat eu adipisicing do quis ea.
+
+Incididunt sit non dolore ea ex minim consequat dolor. Cupidatat exercitation veniam aute do adipisicing elit consequat dolore eu ut cupidatat incididunt. Adipisicing dolore veniam enim eu proident consectetur sit aliquip. Aliqua commodo aliqua do incididunt duis sint labore sint. Ullamco nostrud ea reprehenderit quis ullamco pariatur deserunt cupidatat aliquip laborum. Minim cillum ullamco excepteur amet eu reprehenderit commodo occaecat officia qui anim cupidatat consequat. Nulla qui consectetur Lorem elit do ad.
+
+Ad elit aliqua ut id do sit ipsum ex sunt dolor. Elit eiusmod sunt proident labore voluptate adipisicing. Velit amet tempor amet anim pariatur esse. Adipisicing fugiat ipsum ullamco minim veniam aliquip excepteur ut quis nisi eiusmod enim.
+
+Eu culpa qui in incididunt. Nisi amet dolore Lorem et et consequat. Laboris laborum officia consequat voluptate. Voluptate enim do mollit consectetur proident pariatur eu ut eu reprehenderit.
+
+Nisi ex aliquip ea eu Lorem ex nisi nisi ex enim ut. Proident labore culpa dolore voluptate consequat non reprehenderit laborum dolor culpa laborum eu ea Lorem. Deserunt deserunt consequat est nostrud velit Lorem. Laboris ut laboris minim nulla minim nulla ullamco laborum in ea.
+
+Sunt do cillum dolor aute dolor aute. Minim voluptate minim do consectetur do ex deserunt nisi occaecat aliquip. In ex est excepteur officia do laboris proident qui quis. Irure non pariatur culpa qui occaecat cillum id.
+  </div>
       
-    </div>
+
+      </div>
+
   )
 }
